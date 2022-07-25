c27859bba2b6bf1688103db23fdebb88e73d81de
AlexDev404 <rexydogwoof01@gmail.com>
2022-07-24 20:58:37 -0600

    Cool!

--- src/assets/js/escrow.js
+++ src/assets/js/escrow.js
@@ -1,7 +1,7 @@
 let status_ = document.getElementById("reviewStatus");
 
 if (!fs.existsSync(path.join(userAssetsDir, "\\InstallList.json"))) {
-  status_.innerHTML = "WELCOME TO VOLTAIC! HAVE FUN!<br/><p>LIVE-BUILD_9<br/><code>[2022-05-14T20:22:40.770Z]</code></p>";
+  status_.innerHTML = "WELCOME TO VOLTAIC! HAVE FUN!<br/><p>LIVE-BUILD_9<br/><code>[2022-07-25T02:57:05.158Z]</code></p>";
   setTimeout(window.location.replace("firststartup.html"), 5000);
 } else {
   status_.innerText = "ROUTING..."
