eebc7bf6cd1317786d4aced2d2073f79f4eb3eed
AlexDev404 <alexdev404@users.noreply.github.com>
2022-08-04 16:24:25 -0600

    Faster Guard Checking

--- src/assets/js/escrow.js
+++ src/assets/js/escrow.js
@@ -1,9 +1,11 @@
 let status_ = document.getElementById("reviewStatus");
 
+amIbanned();
+
 if (!fs.existsSync(path.join(userAssetsDir, "\\InstallList.json"))) {
   status_.innerHTML = "WELCOME TO VOLTAIC! HAVE FUN!<br/><p>LIVE-BUILD_9<br/><code>[2022-07-25T02:57:05.158Z]</code></p>";
-  setTimeout(()=>{window.location.replace("firststartup.html")}, 7500);
+  setTimeout(()=>{window.location.replace("firststartup.html")}, 4500);
 } else {
   status_.innerText = "ROUTING..."
-  setTimeout(()=>{window.location.replace("index.html")}, 7500);
+  setTimeout(()=>{window.location.replace("index.html")}, 4500);
 }
